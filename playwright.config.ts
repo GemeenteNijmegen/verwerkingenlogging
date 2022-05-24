a329f91654ae0f8c95fe940e7b29ecfda9674b4f
Joost van der Borg <j.van.der.borg@nijmegen.nl>
2022-05-24 17:30:41 +0200

    remove unused lines

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,15 +1,5 @@
 import type { PlaywrightTestConfig } from '@playwright/test';
-import { devices } from '@playwright/test';
 
-/**
- * Read environment variables from file.
- * https://github.com/motdotla/dotenv
- */
-// require('dotenv').config();
-
-/**
- * See https://playwright.dev/docs/test-configuration.
- */
 const config: PlaywrightTestConfig = {
     testDir: './test/playwright',
     /* Maximum time one test can run for. */
