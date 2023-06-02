278338fc08c1a99df621cd99e42b341f021338a0
Martijn van Dijk <martijn.van.dijk@live.nl>
2023-06-02 16:51:32 +0200

    add table to handler

--- src/GenLambdaFunction/index.py
+++ src/GenLambdaFunction/index.py
@@ -6,7 +6,9 @@ s3 = boto3.resource('s3')
 bucketName = s3.Bucket(os.environ['S3_BACKUP_BUCKET_NAME'])
 sqs = boto3.resource('sqs')
 queue = sqs.Queue(os.environ['SQS_URL'])
+dynamodb = boto3.resource('dynamodb')
+table = dynamodb.Table(os.environ['DYNAMO_TABLE_NAME'])
 
 def handler(event, context):
     print(event)
-    return handle_request(event, bucketName, queue)
+    return handle_request(event, bucketName, queue, table)
