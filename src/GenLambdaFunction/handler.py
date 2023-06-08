e6d78b4901b02c8b24f4aeff664ddd35e6388151
Martijn van Dijk <martijn.van.dijk@live.nl>
2023-06-08 15:05:32 +0200

    bsn fix

--- src/GenLambdaFunction/handler.py
+++ src/GenLambdaFunction/handler.py
@@ -28,11 +28,12 @@ def validate_params(params):
     return params
 
 # Parse message
-def filled_item(requestJSON, actieId, url, tijdstipRegistratie, objectTypeSoortId, compositeSortKey):
+def filled_item(requestJSON, actieId, url, tijdstipRegistratie):
     return {
         'url': url,
         'actieId': actieId,
-        'compositeSortKey': compositeSortKey,
+        'compositeSortKey': '', # filled later
+        'objectTypeSoortId': '', # filled later
         'actieNaam': requestJSON.get('actieNaam'),
         'handelingNaam': requestJSON.get('handelingNaam'),
         'verwerkingId': requestJSON.get('verwerkingId'),
@@ -79,38 +80,64 @@ def store_item_in_s3(actieId, event, bucket):
         Body=data,
     )
 
-# Create a new POST message
-def generate_post_message(requestJson, actieId, url, tijdstipRegistratie, objectTypeSoortId, compositeSortKey, table):
-    item = filled_item(requestJson, actieId, url, tijdstipRegistratie, objectTypeSoortId, compositeSortKey)
-
-    # hash objectId
-    verwerkteObjecten = item.get('verwerkteObjecten')
-    for verwerktObject in verwerkteObjecten:
-        objectId = verwerktObject.get('objectId')
-        hashedObjectId = hashHelper(objectId)
-        verwerktObject.update({ 'objectId': hashedObjectId })
-    
-    # Update item with hashed objectId's
-    item.update({ 'verwerkteObjecten': verwerkteObjecten })
+# Generate id(s) for verwerkteObjecten
+def verwerktObjectId_check(item, table):
+    # cache the created verwerktObjectId for other verwerkteObjecten
+    cachedIds = {}
 
     # Add verwerktObjectId to each verwerktObject before proceeding
     verwerkteObjecten = item.get('verwerkteObjecten')
     if (len(verwerkteObjecten) >= 1):
         for object in verwerkteObjecten:
-
             # check if objectTypeSoortId already exists in DB
+            objectTypeSoortId = object.get('objecttype') + object.get('soortObjectId') + object.get('objectId')
             response = table.query(
-                IndexName='objectTypeSoortId-index',
-                KeyConditionExpression=Key('objectTypeSoortId').eq(objectTypeSoortId))
+                    IndexName='objectTypeSoortId-index',
+                    KeyConditionExpression=Key('objectTypeSoortId').eq(objectTypeSoortId)
+                )
 
-            if (response == None):
+            if ( objectTypeSoortId in cachedIds.keys() ): # validate if verwerkteObjectId was already created before in this function run
+                verwerktObjectId = cachedIds[objectTypeSoortId].get('verwerktObjectId')
+                object.update({ "verwerktObjectId": verwerktObjectId })
+            elif (response.get('Count') == 0):
                 verwerktObjectId = str(uuid.uuid4()) # uuid4 to make uuid random within a for loop (uuid1 gives same uuid to each object)
                 object.update({ "verwerktObjectId": verwerktObjectId })
+                cachedIds.update({ objectTypeSoortId:
+                    { "verwerktObjectId": verwerktObjectId }
+                    })
             else:
+                # search for a verwerkt object (from the query response) where objectTypeSoortId equals that of the main (posted) object
+                # this Id is used to update the item, since we don't want to recreate an Id of an already existing (in DB) verwerkt object.
                 for verwerktObject in response.get('Items')[0].get('verwerkteObjecten'):
                     if (verwerktObject.get('objecttype') + verwerktObject.get('soortObjectId') + verwerktObject.get('objectId') == objectTypeSoortId):
                         verwerktObjectId = verwerktObject.get('verwerktObjectId')
-                        object.update({ "verwerktObjectId": verwerktObjectId })
+                
+                # update verwertkObjectId
+                object.update({ "verwerktObjectId": verwerktObjectId })
+
+    return item
+
+# Hash the obejctId
+def objectId_check(item):
+    for object in item.get('verwerkteObjecten'):
+        # hash objectId, create composite sort key and update item
+        objectId = object.get('objectId')
+        if (len(objectId) < 10): # dirty fix to check whether BSN has already been hashed
+            hashedObjectId = hashHelper(objectId)
+        else:
+            hashedObjectId = objectId
+        
+        object.update({'objectId': hashedObjectId})
+
+    return item
+
+# Create a new POST message
+def generate_post_message(verwerktObject, requestJson, actieId, url, tijdstipRegistratie):
+    item = filled_item(requestJson, actieId, url, tijdstipRegistratie)
+    
+    objectTypeSoortId = verwerktObject.get('objecttype') + verwerktObject.get('soortObjectId') + verwerktObject.get('objectId')
+    compositeSortKey = objectTypeSoortId + '#' + tijdstipRegistratie   # Composite SK - combining the unique soortObjecTypeId and the tijdstipRegistratie timestamp
+    item.update({ 'compositeSortKey': compositeSortKey, 'objectTypeSoortId': objectTypeSoortId })
 
     return validate_body(item)
 
@@ -164,19 +191,21 @@ def handle_request(event, bucket, queue, table):
         # Create DB url using generated actieId
         url = "https://verwerkingenlogging-bewerking-api.vng.cloud/api/v1/verwerkingsacties/" + actieId
 
-        for verwerktObject in requestJson.get('verwerkteObjecten'):
-            objectTypeSoortId = verwerktObject.get('objecttype') + verwerktObject.get('soortObjectId') + verwerktObject.get('objectId')
-            # Composite SK - combining the unique soortObjecTypeId and the tijdstipRegistratie timestamp
-            compositeSortKey = objectTypeSoortId + '#' + tijdstipRegistratie
+        item = objectId_check(requestJson)
+        item = verwerktObjectId_check(item, table)
+
+        for verwerktObject in item.get('verwerkteObjecten'):
+
             # Generate post message (including verwerktObjectId)
-            msg = generate_post_message(requestJson, actieId, url, tijdstipRegistratie, objectTypeSoortId, compositeSortKey, table)
+            msg = generate_post_message(verwerktObject, item, actieId, url, tijdstipRegistratie)
 
             # Send message to queue
             send_to_queue(msg, queue, 'POST')
 
         # Message inlcudes original request combined with actieId and Url
-        # Remove objectTypeSoortId from return message
+        # Remove compositeSortKey and objectTypeSoortId from return message
         msg.pop('compositeSortKey')
+        msg.pop('objectTypeSoortId')
         return { 'statusCode': 200, 'body': json.dumps(msg), 'headers': { "Content-Type": "application/json" }}
 
     if(params.get('method') == 'PATCH' and params.get('resource') =='/verwerkingsacties'):
