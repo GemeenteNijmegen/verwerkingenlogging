40dc1389a681c08b6cb491dfe7a47001df01728f
Marnix Dessing <m.dessing@nijmegen.nl>
2023-08-31 10:29:34 +0200

    fix: make accp default build branch

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { App } from 'aws-cdk-lib';
 import { getConfiguration } from './Configuration';
 import { PipelineStack } from './PipelineStack';
 
-const buildBranch = process.env.BRANCH_NAME ?? 'development';
+const buildBranch = process.env.BRANCH_NAME ?? 'acceptance';
 console.log('Building branch', buildBranch);
 const configuration = getConfiguration(buildBranch);
 
